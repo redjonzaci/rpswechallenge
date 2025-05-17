4edcc5476f4e44bacf8dc31de9249f5b7dbb4972
redjonzaci <redjonzaci@gmail.com>
2025-05-18 01:55:36 +0200

    make UI dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,64 @@
 import { post } from "aws-amplify/api";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [packSizes, setPackSizes] = useState<string>("");
+  const [amount, setAmount] = useState<string>("");
+  const [orderPacks, setOrderPacks] = useState<{ [key: number]: number }>({});
+
   return (
     <>
       <button
-        onClick={() => {
-          post({
+        onClick={async () => {
+          const { body } = await post({
             apiName: "orderpackscalculator",
             path: "/calculate",
             options: {
               body: {
-                packSizes: [23, 31, 53],
-                amount: 500000,
+                packSizes: packSizes.split(",").map(Number),
+                amount: Number(amount),
               },
             },
-          }).response.then((res) => {
-            console.log(res);
-          });
+          }).response;
+
+          const calculateResponse = (await body.json()) as {
+            orderPacks: { [key: number]: number };
+          };
+
+          setOrderPacks(calculateResponse.orderPacks);
         }}
       >
         Calculate
       </button>
+
+      <div>
+        <label htmlFor="packSizes">Pack Sizes (comma separated)</label>
+        <input
+          id="packSizes"
+          type="text"
+          value={packSizes}
+          onChange={(e) => setPackSizes(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="amount">Amount (integer)</label>
+        <input
+          id="amount"
+          type="text"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+      </div>
+
+      <div>
+        {Object.entries(orderPacks).map(([packSize, quantity]) => (
+          <div key={packSize}>
+            {packSize} x {quantity}
+          </div>
+        ))}
+      </div>
     </>
   );
 }
